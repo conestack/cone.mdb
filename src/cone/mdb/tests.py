878ad947abdc2f863fe25bcda6da9fca333fc789
Robert Niederreiter <rnix@squarewave.at>
2011-03-09 16:56:56 +0100

    initial buildout

---
+++ src/cone/mdb/tests.py
@@ -0,0 +1,37 @@
+import doctest
+import interlude
+import pprint
+import unittest2 as unittest
+from plone.testing import layered
+from cone.app import testing
+
+optionflags = doctest.NORMALIZE_WHITESPACE | \
+              doctest.ELLIPSIS | \
+              doctest.REPORT_ONLY_FIRST_FAILURE
+
+layer = testing.Security()
+
+TESTFILES = [
+    #'foo.txt',
+]
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(
+            doctest.DocFileSuite(
+                testfile,
+                globs={'interact': interlude.interact,
+                       'pprint': pprint.pprint,
+                       'pp': pprint.pprint,
+                       },
+                optionflags=optionflags,
+                ),
+            layer=layer,
+            )
+        for testfile in TESTFILES
+        ])
+    return suite
+
+if __name__ == '__main__':                                  #pragma NO COVERAGE
+    unittest.main(defaultTest='test_suite')                 #pragma NO COVERAGE
